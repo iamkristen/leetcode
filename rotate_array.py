9324e252951ef27b90e6b05f99545bdc7e195f18
Kristen <iamkristen220@gmail.com>
2025-05-29 23:53:15 +0100

    rotate array

---
+++ rotate_array.py
@@ -0,0 +1,12 @@
+def rotate_left(arr, d):
+    d %= len(arr)
+    return arr[d:] + arr[:d]
+
+
+arr1 = [1, 2, 3, 4, 5, 6]
+d1 = 2
+print(rotate_left(arr1, d1))  # Output: [3, 4, 5, 6, 1, 2]
+
+arr2 = [1, 2, 3]
+d2 = 4
+print(rotate_left(arr2, d2))  # Output: [2, 3, 1]
